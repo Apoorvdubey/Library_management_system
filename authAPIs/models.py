ed397e1cefec087d5909916f231ffe507cb4bdd9
Sakib Ali <alisakib899@gmail.com>
2022-03-26 15:14:11 +0530

    donation apis

--- authAPIs/models.py
+++ authAPIs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+from users.models import Users
 # Create your models here.
 
 class Banners(models.Model):
@@ -9,3 +10,19 @@ class Banners(models.Model):
     createdAt = models.DateTimeField(default=now, editable=False)
     class Meta:
         db_table='banners'
+
+class UserDonations(models.Model):
+    paymentStatus = ((1,"pending"),(2,"completed"),(3,"canceled"),(4,"failed"))
+
+    donationId = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(Users, on_delete=models.CASCADE, default=None, blank=True, related_name='userDonationDetail', db_column='userId')
+    transactionId = models.CharField(max_length=255)
+    payerId = models.CharField(max_length=255)
+    payerEmail = models.CharField(max_length=255)
+    paymentStatus = models.IntegerField(choices=paymentStatus,null=False,default=2)
+    transactionMode = models.CharField(max_length=255)
+    paymentAmount = models.CharField(max_length=255)
+    createdAt = models.DateTimeField(default=now, editable=False)
+    updatedAt = models.DateTimeField(default=now, editable=False)
+    class Meta:
+        db_table='user_donations'
